54ebfc8314858b496c938346d5869d1f95bc3ffc
Jennifer Nguyen <jennnguyen@csumb.edu>
2017-11-08 04:26:09 +0000

    created table

--- shoppingCart.php
+++ shoppingCart.php
@@ -5,14 +5,27 @@ function displayCart() {
     include 'dbConnections.php';
     $conn = getDatabaseConnection();
     $array = $_SESSION['vgID'];
+    
+    
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Game</th>";
+    echo "<th>Price</th>";
+    echo "</tr>";
+          
     foreach($array as $a){
         $sql = "SELECT * FROM gp2_game WHERE vgID = $a";
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         $records = $stmt->fetch(PDO::FETCH_ASSOC);
-        echo "Name: ". $records['name'] . "     Price: ". $records['price'] . "<br/><br/>";
-        }
+        echo "<tr>";
+        echo "<td>".ucwords($records['name'])."</td>";
+        echo "<td>".ucwords($records['price'])."</td>";
+        echo "</tr>";
     }
+    echo "</table>";
+}
+    
 ?>
 
 <!DOCTYPE html>
